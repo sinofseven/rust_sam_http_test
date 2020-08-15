15891522d86b1dd708e58e324de0243c770e39e9
yuta-natsume <natsume.yuta@classmethod.jp>
2020-08-15 18:48:11 +0900

    echo event

--- src/bin/search.rs
+++ src/bin/search.rs
@@ -1,17 +1,31 @@
-use lambda_http::{handler, lambda, Context, IntoResponse, Request, Response};
+use lambda_http::{handler, lambda, Context, IntoResponse, Request, RequestExt, Response};
+use std::collections::HashMap;
 
 type Error = Box<dyn std::error::Error + Sync + Send + 'static>;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    println!("test");
     lambda::run(handler(hello)).await?;
     Ok(())
 }
 
-async fn hello(_: Request, _: Context) -> Result<impl IntoResponse, Error> {
+fn parse_query(event: &Request) -> HashMap<String, String> {
+    let mut map: HashMap<String, String> = HashMap::new();
+    let query = event.query_string_parameters();
+    for (k, v) in query.iter() {
+        println!("k={}, v={}", k, v);
+    }
+
+    map
+}
+
+async fn hello(event: Request, _: Context) -> Result<impl IntoResponse, Error> {
     // `serde_json::Values` impl `IntoResponse` by default
     // creating an application/json response
-    println!("aaa");
-    Ok(Response::builder().status(200).body("test").expect("failed"))
+    let _ = parse_query(&event);
+    Ok(Response::builder()
+        .status(200)
+        .header("Content-Type", "application/json")
+        .body(serde_json::to_string(&event).unwrap())
+        .expect("failed"))
 }
