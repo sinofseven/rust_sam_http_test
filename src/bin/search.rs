d09d5a9c6ae09cf80be3cf3d1d25bc757f327e0a
yuta-natsume <natsume.yuta@classmethod.jp>
2020-08-15 17:48:16 +0900

    fix response

--- src/bin/search.rs
+++ src/bin/search.rs
@@ -1,4 +1,4 @@
-use lambda_http::{handler, lambda, Context, IntoResponse, Request};
+use lambda_http::{handler, lambda, Context, IntoResponse, Request, Response};
 use serde_json::json;
 
 type Error = Box<dyn std::error::Error + Sync + Send + 'static>;
@@ -12,7 +12,5 @@ async fn main() -> Result<(), Error> {
 async fn hello(_: Request, _: Context) -> Result<impl IntoResponse, Error> {
     // `serde_json::Values` impl `IntoResponse` by default
     // creating an application/json response
-    Ok(json!({
-        "message": "Go Serverless v1.0! Your function executed successfully!"
-    }))
+    Ok(Response::builder().status(200).body("test").expect("failed"))
 }
